2ea5010058b659bc18da7a9f0b236b44ee4d1bfe
bautzch <bautzch@10.20.173.30>
2017-05-29 11:55:02 +0200

    Updates Projekt

---
+++ src/de/mcbc/karelui/KarelUI.java
@@ -0,0 +1,14 @@
+package de.mcbc.karelui;
+
+import karel.swingui.KarelFrame;
+
+public class KarelUI {
+
+	public static void main(String[] args) {
+		KarelFrame frame = new KarelFrame("karel"); 
+		
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+	
+}
